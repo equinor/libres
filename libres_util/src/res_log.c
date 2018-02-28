7b5ea2b099065d3490a7cb8e5641144049fa4af1
Pål Grønås Drange <PGDR@statoil.com>
2018-02-28 16:15:37 +0100

    Refactor out subfunctions

--- libres_util/src/res_log.c
+++ libres_util/src/res_log.c
@@ -1,31 +1,26 @@
 /*
-   Copyright (C) 2014  Statoil ASA, Norway. 
-    
+   Copyright (C) 2014  Statoil ASA, Norway.
+
    The file 'res_log.c' is part of ERT - Ensemble based Reservoir Tool.
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
-/*TODO:
- * For libres:
- * - Go over the current log-calls and make sure they use reasonable log-levels
- * For libecl:
- * - Change log.h to use the normal semantics of log-levels, where higher means more important message.
- * - Change log.h to use the same numeric values for log-levels as Python
- * - Should we insert ERROR/WARNING etc in the front of the messages?
- * - Should we actually have these as macros which can then insert the filename/line-number in the message?
- * For both:
- * - Change all occurrences of ints to represent log-level to actually use the message_level_type?
+/*
+ * TODO:
+ *
+ * Should we have these as macros which can then insert the
+ * filename/line-number in the message?
  */
 
 #include <stdarg.h>
@@ -119,10 +114,12 @@ void res_log_fcritical(const char * fmt, ...) {
 
 
 /**
- * The logging uses log_level to determine if an incoming message is to be included in the log.
- * A high log_level setting will include more messages.
+ *  The logging uses log_level to determine if an incoming message is to be
+ *  included in the log.
  *
- * if log_file_name=NULL then DEFAULT_LOG_FILE is used
+ *  A high log_level setting will include more messages.
+ *
+ *  if log_file_name=NULL then DEFAULT_LOG_FILE is used
  */
 void res_log_init_log(message_level_type log_level,
                       const char * log_file_name,
@@ -137,7 +134,7 @@ void res_log_init_log(message_level_type log_level,
   logh = log_open(log_file_name , log_level );
 
   if (verbose)
-    printf("Activity will be logged to ..............: %s \n",log_get_filename( logh ));
+    printf("Activity will be logged to %s \n", log_get_filename(logh));
   log_add_message(logh, LOG_INFO, NULL, "ert configuration loaded");
 }
 
@@ -146,7 +143,7 @@ void res_log_init_log(message_level_type log_level,
  * If log_file_name=NULL then DEFAULT_LOG_FILE is used
  */
 void res_log_init_log_default_log_level(const char * log_file_name, bool verbose){
-  res_log_init_log(DEFAULT_LOG_LEVEL,log_file_name,verbose);
+  res_log_init_log(DEFAULT_LOG_LEVEL, log_file_name, verbose);
 }
 
 
