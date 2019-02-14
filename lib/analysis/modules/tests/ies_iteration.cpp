1c93fc6aa208f45806f45f18bd03c1338c595b24
Joakim Hove <joakim.hove@gmail.com>
2019-02-14 18:02:23 +0100

    WIP

--- lib/analysis/modules/tests/ies_iteration.cpp
+++ lib/analysis/modules/tests/ies_iteration.cpp
@@ -33,25 +33,25 @@ void init_stdA(const res::es_testdata& testdata, matrix_type * A2) {
 
 
 void cmp_std_ies(const res::es_testdata& testdata) {
-  int num_iter = 5;
+  int num_iter = 25;
   rng_type * rng = rng_alloc( MZRAN, INIT_DEFAULT );
   matrix_type * A1 = testdata.alloc_state("prior");
   matrix_type * A2 = testdata.alloc_state("prior");
 
-  ies_enkf_data_type * ies_data1 = static_cast<ies_enkf_data_type*>(ies_enkf_data_alloc(rng));
-  ies_enkf_config_type * ies_config1 = ies_enkf_data_get_config(ies_data1);
+  ies_enkf_data_type * ies_data = static_cast<ies_enkf_data_type*>(ies_enkf_data_alloc(rng));
+  ies_enkf_config_type * ies_config = ies_enkf_data_get_config(ies_data);
 
   bool_vector_type * ens_mask = bool_vector_alloc(testdata.active_ens_size, true);
   bool_vector_type * obs_mask = bool_vector_alloc(testdata.active_obs_size, true);
 
-  ies_enkf_config_set_truncation(ies_config1, 0.95);
-  ies_enkf_config_set_ies_steplength(ies_config1, 0.50);
-  ies_enkf_config_set_ies_inversion(ies_config1, IES_INVERSION_SUBSPACE_EXACT_R);
-  ies_enkf_config_set_ies_aaprojection(ies_config1, false);
+  ies_enkf_config_set_truncation(ies_config, 0.95);
+  ies_enkf_config_set_ies_steplength(ies_config, 0.50);
+  ies_enkf_config_set_ies_inversion(ies_config, IES_INVERSION_SUBSPACE_EXACT_R);
+  ies_enkf_config_set_ies_aaprojection(ies_config, false);
 
 
   for (int iter=0; iter < num_iter; iter++) {
-    ies_enkf_init_update(ies_data1,
+    ies_enkf_init_update(ies_data,
                          ens_mask,
                          obs_mask,
                          testdata.S,
@@ -61,9 +61,7 @@ void cmp_std_ies(const res::es_testdata& testdata) {
                          testdata.D,
                          rng);
 
-    // Does this update the iter count+++
-
-    ies_enkf_updateA(ies_data1,
+    ies_enkf_updateA(ies_data,
                      A1,
                      testdata.S,
                      testdata.R,
@@ -72,16 +70,20 @@ void cmp_std_ies(const res::es_testdata& testdata) {
                      testdata.D,
                      NULL,
                      rng);
+
+    test_assert_int_equal( ies_enkf_data_get_iteration_nr(ies_data), iter + 1);
   }
 
   init_stdA(testdata, A2);
-  test_assert_true( matrix_similar(A1, A2, 5e-6));
+  matrix_pretty_fprint(A1, "   IES", " %6.3f", stdout);
+  matrix_pretty_fprint(A2, "Std ES", " %6.3f", stdout);
+  test_assert_true( matrix_similar(A1, A2, 1e-3));
 
   bool_vector_free(ens_mask);
   bool_vector_free(obs_mask);
   matrix_free(A1);
   matrix_free(A2);
-  ies_enkf_data_free(ies_data1);
+  ies_enkf_data_free(ies_data);
   rng_free( rng );
 }
 
